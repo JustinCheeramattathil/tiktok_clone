c75605e4490ab8df06799a27231365cfcb1f61a9
Justin Thomas <thomasjustin2203@gmail.com>
2024-01-07 10:35:12 +0530

    firebase installed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:tiktok_clone/constants.dart';
-import 'package:tiktok_clone/views/screens/auth/login_screen.dart';
+import 'package:tiktok_clone/firebase_options.dart';
 import 'package:tiktok_clone/views/screens/auth/signup_screen.dart';
 
-void main() {
+void main()async {
+    WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -12,7 +18,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
